a39c8876cbe531be9d6d43a7a9cff8c45d09c3df
Dapple1990 <bone.dalma@gmail.com>
2019-06-23 15:42:12 +0200

    refactor : code cleanup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Display from './display.js'
-import Button from './button'
+import Display from './components/display';
+import Button from './components/button';
+import { connect } from 'react-redux';
+
 
 class App extends React.Component {
-  state = {
-    counter: 0,
-  }
 
   componentDidMount = () => {
     document.addEventListener('keyup', (event) => {
@@ -14,33 +13,36 @@ class App extends React.Component {
       } else if (event.keyCode === 40) {
         this.decrCounter()
       }
-    });
+    })
   };
 
+
   incrCounter = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    })
+    this.props.dispatch({type : 'buy'})
   }
 
   decrCounter = () => {
-    if (this.state.counter !== 0) {
-      this.setState({
-        counter: this.state.counter - 1
-      })
+    if(this.props.count > 0){
+      this.props.dispatch({type : 'eat'})
     }
-  }
+  };
+
 
   render() {
     return (<div>
-      <Button handleClick={this.incrCounter} name="Buy One" />
-      <Display numberOfAcorns={this.state.counter} />
-      <Button handleClick={this.decrCounter} name="Eat One" />
+      <Button onClick={this.incrCounter} name="Buy One" />
+      <Display numberOfAcorns={this.props.count} />
+      <Button onClick={this.decrCounter} name="Eat One" />
     </div>
     );
 
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    count : state.counter
+  };
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
