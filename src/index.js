97f90d66496c6566c882c1003594df5bbbb83b16
Dapple1990 <bone.dalma@gmail.com>
2019-06-24 21:26:08 +0200

    feat(): add router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-import {Provider} from 'react-redux';
+import AcornRedux from './app';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducer/rootReducer';
+import { BrowserRouter } from 'react-router-dom'
+import App from './app';
+import AcornReact from './components/acornReact';
+import { Route } from "react-router-dom";
+import NavBar from './components/navBar';
 
 const store = createStore(reducer);
 
 ReactDOM.render((
-  <Provider store={store}>
-    <App />
-  </Provider>
-), 
-document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={store}>
+      <NavBar />
+      <Route path='/simple/states' component={AcornReact} />
+      <Route path='/simple/redux' component={AcornRedux} />
+    </Provider>
+  </BrowserRouter>
+),
+  document.getElementById('root'));
 
 /* ReactDOM.render(
   <h1>Hello, Green Fox Academy!</h1>,
