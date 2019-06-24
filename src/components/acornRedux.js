4e7b190eddddbccec255d57fc49aae267be03dfe
Dapple1990 <bone.dalma@gmail.com>
2019-06-24 21:27:57 +0200

    feat(): add exercise with Redux

---
+++ src/components/acornRedux.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Display from './display';
+import Button from './button';
+import { connect } from 'react-redux';
+
+class AcornRedux extends React.Component {
+
+  componentDidMount = () => {
+    document.addEventListener('keyup', (event) => {
+      if (event.keyCode === 38) {
+        this.incrCounter()
+      } else if (event.keyCode === 40) {
+        this.decrCounter()
+      }
+    })
+  };
+
+  incrCounter = () => {
+    this.props.dispatch({type : 'buy'})
+  }
+
+  decrCounter = () => {
+    if(this.props.count > 0){
+      this.props.dispatch({type : 'eat'})
+    }
+  };
+
+  render() {
+    return (<div>
+      <Button onClick={this.incrCounter} name="Buy One" />
+      <Display numberOfAcorns={this.props.count} />
+      <Button onClick={this.decrCounter} name="Eat One" />
+    </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    count : state.counter
+  };
+}
+
+export default connect(mapStateToProps)(AcornRedux);
