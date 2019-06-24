9b50dfc5cc8f3010bb6d9f9c093da2e64df55a6a
Dapple1990 <bone.dalma@gmail.com>
2019-06-24 21:27:20 +0200

    feat(): add exercise with states

---
+++ src/components/acornReact.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Display from './display.js'
+import Button from './button'
+
+class AcornReact extends React.Component {
+
+  state = {
+    counter : 0,
+  }
+  
+  componentDidMount = () => {
+    document.addEventListener('keyup', (event) => {
+      if (event.keyCode === 38) {
+        this.incrCounter()
+      } else if (event.keyCode === 40) {
+        this.decrCounter()
+      }
+    })
+  };
+
+
+  incrCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrCounter = () => {
+    this.state.counter !== 0 ? this.setState({ counter: this.state.counter - 1 }) : null
+  };
+
+
+  render() {
+    console.log(this.state)
+    return (<div>
+      <Button onClick={this.incrCounter} name="Buy One" />
+      <Display numberOfAcorns={this.state.counter} />
+      <Button onClick={this.decrCounter} name="Eat One" />
+    </div>
+    );
+
+  }
+}
+
+
+export default AcornReact;
